4839f2132cbecb580d75b0092fbce641948b1c82
Sergiu Nuta <sergiu.nuta@gmail.com>
2021-08-03 19:54:56 +0100

    working if condition on sorting per groups

--- src/app/new-things-list/new-things-list.component.ts
+++ src/app/new-things-list/new-things-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import newThingsData from "../data/newThings.json";
 
 interface newThing {
+  id: Number;
   areaId: Number;
+  joinedWith: any;
   sku: String;
   defaultSku: String;
 }
